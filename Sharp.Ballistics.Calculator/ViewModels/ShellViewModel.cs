8dd22c1d4db5bcd6c8e1735c5842f67099f9f863
Michael Yarichuk <michael.yarichuk@gmail.com>
2015-12-11 01:28:02 +0200

    skeleton of configuration window stuff

--- Sharp.Ballistics.Calculator/ViewModels/ShellViewModel.cs
+++ Sharp.Ballistics.Calculator/ViewModels/ShellViewModel.cs
@@ -6,10 +6,23 @@ namespace Sharp.Ballistics.Calculator.ViewModels
     public class ShellViewModel : Conductor<FunctionScreen>
     {
         private readonly IWindowManager windowManager;
-
+        private FunctionScreen currentFunction;
         public ShellViewModel(IWindowManager windowManager)
         {
             this.windowManager = windowManager;
+        }
+
+        public FunctionScreen CurrentFunction
+        {
+            get
+            {
+                return currentFunction;
+            }
+            set
+            {
+                currentFunction = value;
+                NotifyOfPropertyChange(() => CurrentFunction);
+            }
         }
     }
 }
