b5752299f7f3db492755f7562ee65ba2c8ca3efc
Michael Yarichuk <michael.yarichuk@gmail.com>
2016-01-04 10:33:51 +0200

    minor

--- Sharp.Ballistics.Calculator/Converters/FieldValueConverter.cs
+++ Sharp.Ballistics.Calculator/Converters/FieldValueConverter.cs
@@ -2,15 +2,9 @@
 using Sharp.Ballistics.Calculator.Bootstrap;
 using Sharp.Ballistics.Calculator.Models;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Data;
-using UnitsNet;
 
 namespace Sharp.Ballistics.Calculator
 {
--- Sharp.Ballistics.Calculator/ViewModels/SingleCalculationResultViewModel.cs
+++ Sharp.Ballistics.Calculator/ViewModels/SingleCalculationResultViewModel.cs
@@ -1,10 +1,5 @@
 ﻿using Caliburn.Micro;
 using Sharp.Ballistics.Abstractions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using GNUBallistics = GNUBallisticsLibrary;
 using Sharp.Ballistics.Calculator.Models;
 using UnitsNet;
